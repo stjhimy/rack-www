3162430814d0e9b18698bdee3e13e7d7b234ad3d
stJhimy <stjhimy@gmail.com>
2011-05-04 22:09:54 -0300

    That's it for now

--- lib/go_www.rb
+++ lib/go_www.rb
@@ -5,6 +5,17 @@ module GoWww
   end
 
   module ClassMethods
+    def already_www?
+      return if request.host.downcase =~ /^(www.)/
+    end
+
+    def ensure_www
+      if RAILS_ENV == "production" &&  !already_www?
+        redirect_to "#{request.protocol + 'www.' + request.host + request.path}"
+        flash.keep
+        return false
+      end
+    end
   end
   
 end
