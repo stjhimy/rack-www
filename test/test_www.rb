707dc0f3e6f0aaa3b989e88b580ec394655cbd35
stJhimy <stjhimy@gmail.com>
2011-05-09 10:17:57 -0300

    Tests moved to ActiveSupport::TestCase

--- test/test_www.rb
+++ test/test_www.rb
@@ -1,16 +1,17 @@
 require "rubygems"
 require "rack/www"
 require 'test/unit'
+require 'active_support'
 require 'rack/test'
 
-class TestWWW < Test::Unit::TestCase
+class TestWWW < ActiveSupport::TestCase
   include Rack::Test::Methods
 
   def default_app
     lambda { |env|
       headers = {'Content-Type' => "text/html"}
       headers['Set-Cookie'] = "id=1; path=/\ntoken=abc; path=/; secure; HttpOnly"
-      [200, headers, ["Only WWW!"]]
+      [200, headers, ["default body"]]
     }
   end
 
@@ -19,48 +20,48 @@ class TestWWW < Test::Unit::TestCase
   end
   attr_writer :app
 
-  def test_response_is_ok
+  test "response has status 200[ok] in the default request" do
     get "http://www.example.com"
-    assert last_response.ok?
+    assert_equal last_response.status, 200
   end
 
-  def test_redirects_when_not_www
+  test "response has status 301[redirects] when not a www url" do
     get "http://example.com"
-    assert last_response.redirect?
+    assert_equal last_response.status, 301
   end
 
-  def test_right_location_when_redirects
+  test "Redirects to right location when it's not www url" do
     get "http://example.com/"
     assert_equal "http://www.example.com/", last_response.headers['Location']
   end
 
-  def test_param_www
+  test "Redirects to a www url when param :www => true" do
     self.app = Rack::WWW.new(default_app, :www => true)
     get "http://example.com/"
     assert_equal "http://www.example.com/", last_response.headers['Location']
   end
 
-  def test_param_www_false_when_www
+  test "Redirects to a non www url when param :www => false" do
     self.app = Rack::WWW.new(default_app, :www => false)
     get "http://www.example.com/"
     assert_equal "http://example.com/", last_response.headers['Location']
   end
 
-  def test_param_www_false_when_not_www
+  test "Keeps the same url when non www url and param :www => false" do
     self.app = Rack::WWW.new(default_app, :www => false)
     get "http://example.com/"
     assert last_response.ok?
   end
 
-  def test_param_message
+  test "Changes the body content when param :message" do
     self.app = Rack::WWW.new(default_app, :www => true, :message => "redirecting now!")
     get "http://example.com/"
     assert_equal last_response.body, "redirecting now!"
   end
 
-  def test_when_not_param_message
+  test "Keeps the body content when not param :message" do
     self.app = Rack::WWW.new(default_app, :www => true)
     get "http://example.com/"
-    assert_equal last_response.body, "Only WWW!"
+    assert_equal last_response.body, "default body"
   end
 end
