5d9aaca912c09eb96ae08b55d3752bf045a2f8ae
stJhimy <stjhimy@gmail.com>
2011-05-09 08:44:44 -0300

    Changed default_app in tests

--- test/test_www.rb
+++ test/test_www.rb
@@ -6,13 +6,16 @@ require 'rack/test'
 class TestWWW < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def app
-   new_app = lambda { |env|
+  def default_app
+    lambda { |env|
       headers = {'Content-Type' => "text/html"}
       headers['Set-Cookie'] = "id=1; path=/\ntoken=abc; path=/; secure; HttpOnly"
       [200, headers, ["Only WWW!"]]
     }
-    @app ||= Rack::WWW.new(new_app)
+  end
+
+  def app
+    @app ||= Rack::WWW.new(default_app)
   end
   attr_writer :app
 
@@ -29,6 +32,10 @@ class TestWWW < Test::Unit::TestCase
   def test_right_location_when_redirects
     get "http://example.com/"
     assert_equal "http://www.example.com/", last_response.headers['Location']
+  end
+
+  def test_param_www
+    self.app = Rack::WWW.new(default_app, :www => true)
   end
 
 end
